a6a6710bb9cf8b5bfe3462470055c2f5896cc9c3
daviiid20 <davidjimenez92@gmail.com>
2022-04-17 20:12:28 +0200

    Added repository contract with unit test close #6

---
+++ VuelingPractica.Infrastructure.Repositories/Contracts/IAdd.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VuelingPractica.Infrastructure.Repositories.Contracts
+{
+    public interface IAdd<T>
+    {
+        T Add(T entity);
+    }
+}
